46de093740aa0c49eb047205cb5fb62c21478236
ZuluPhiwa <phiwazulu43@gmail.com>
2024-03-12 14:58:42 +0200

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import { Outlet } from "react-router";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Outlet />
     </>
   );
 }
